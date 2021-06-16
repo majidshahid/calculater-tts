3f45c5224994568da7b614dba74505d9493d4a58
majid shahid <majidshahid474@gmail.com>
2021-06-16 13:48:01 -0700

    js calculator

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-function number(num){
- var input= document.getElementById('input');
- input.value +=num
+// function number(num) {
+//     var input = document.getElementById('input');
+//     var lastindex = input.value.slice(-1);
+//     var opt = ["+", "-", "/", "*"];
+//     if (opt.indexOf(lastindex) !== -1 && opt.indexOf(num) !== -1) {
+//         input.value = input.value.slice(0, -1) + num
+//     } else {
+//         input.value += num
+//     }
 
+// }
+
+function number (num){
+    var input= document.getElementById("input");
+    var lastindex = input.value.slice(-1);
+    var opt = ["+","-","/","*"];
+    if (opt.indexOf(lastindex) !== -1 && opt.indexOf(num) !== -1){
+        input.value =input.value.slice(0,-1) + num
+    }else{
+        input.value += num
+    }
 }
-
-function result(){
+function result() {
     var result = document.getElementById("input");
-   if(result.value ==""){
-result.value=''
-   }else{
-       result.value=eval(result.value)
-   }
+    if (result.value == "") {
+        result.value = ''
+    } else {
+        result.value = eval(result.value)
+    }
 }
-function clearinput(){
-document.getElementById("input").value=""
+function clearinput() {
+    document.getElementById("input").value = ""
 }
-function delet(){
-    document.getElementById("input").value=document.getElementById("input").value.toString().slice(0,-1);
+function delet() {
+    document.getElementById("input").value = document.getElementById("input").value.toString().slice(0, -1);
 }
